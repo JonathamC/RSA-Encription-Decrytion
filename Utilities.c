38e700ac1df71f69f3252ff7300e17ca9e53c17d
JonathamC <86174437+JonathamC@users.noreply.github.com>
2022-11-24 18:19:55 -0500

    update

--- Utilities.c
+++ Utilities.c
@@ -1,6 +1,7 @@
 #include <stdio.h> 
 #include <string.h> 
 #include <stdlib.h>
+#include <time.h>
 
 const char* stringToInt(char message[]){
     int lenMessage = strlen(message) * 3; 
@@ -52,12 +53,62 @@ char* substring(char *destination, const char *source, int beg, int n)
     return plainText;
 
  }
-// int main(){
-//     char message[] = "Hi! Let's go to the playground!";
-//     const char* cipherText = stringToInt(message);
-//     printf("\nCiphertext = %s\n\n", cipherText);
 
-//     const char* plainText = intToString(cipherText);
-//     printf("\nPlaintext = %s\n", plainText);
-//     return 0; 
-// }
+
+int coprime(int num1, int num2)  
+{  
+    int min, count, found = 1;  
+    min = num1 < num2 ? num1 : num2;  
+    for(count = 2; count <= min; count++)  
+    {  
+        if( num1 % count == 0 && num2 % count == 0 ) 
+        {  
+            found = 0;  
+            break;  
+        }  
+    }  
+    return(found);  
+}
+
+int check_prime(int a)
+{
+   int c;
+ 
+   for ( c = 2 ; c <= a - 1 ; c++ )
+   { 
+      if ( a%c == 0 )
+     return 0;
+   }
+   return 1; // is prime
+}
+
+int RNG_prime(){
+    int i, n;
+    time_t t;
+    /* Intializes random number generator */
+    srand((unsigned) time(&t));
+    /* random numbers from 0 to 1000 */
+    int randomInt = rand() % 1000;
+    if (check_prime(randomInt) == 1){
+        return randomInt;
+    }
+    else { 
+        while(1){
+            randomInt = rand() % 1000;
+            if (check_prime(randomInt) == 1){
+                return randomInt;
+            }
+        }
+    }
+}
+int main(){
+    // char message[] = "Hi! Let's go to the playground!";
+    // const char* cipherText = stringToInt(message);
+    // printf("\nCiphertext = %s\n\n", cipherText);
+
+    // const char* plainText = intToString(cipherText);
+    // printf("\nPlaintext = %s\n", plainText);
+    int p = RNG_prime();
+    printf("%d\n", p);
+    return 0; 
+}
