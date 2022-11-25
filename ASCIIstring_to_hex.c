7b2a9d4449be68c56e8a109b5300e50fcecfaef3
JonathamC <86174437+JonathamC@users.noreply.github.com>
2022-11-25 18:57:08 -0500

    update

--- ASCIIstring_to_hex.c
+++ ASCIIstring_to_hex.c
@@ -1,5 +1,20 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <openssl/bn.h>
 
-int main(){
-    printf("hello");
+#define NBITS 256
+void printBN(char *msg, BIGNUM * a)
+{
+/* Use BN_bn2hex(a) for hex string
+* Use BN_bn2dec(a) for decimal string */
+    char * number_str = BN_bn2hex(a);
+    printf("%s %s\n", msg, number_str);
+    OPENSSL_free(number_str);
+}
+int main ()
+{
+    BN_CTX *ctx = BN_CTX_new();
+    BIGNUM *a = BN_new();
+    BIGNUM *b = BN_new();
+    BIGNUM *n = BN_new();
+    BIGNUM *res = BN_new();
 }
